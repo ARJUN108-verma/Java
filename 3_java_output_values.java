1e5e39444690d02708ce2902d4181ab1daff8742
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-26 11:14:44 +0530

    Update 3_java_output_values.java

--- 3_java_output_values.java
+++ 3_java_output_values.java
@@ -1,1 +1,22 @@
+public class OutputExample {
+    public static void main(String[] args) {
+        // Printing text
+        System.out.println("Hello, Java!");
+
+        // Printing numbers
+        int number = 10;
+        double pi = 3.14159;
+
+        System.out.println("Integer number: " + number);
+        System.out.println("Double value of Pi: " + pi);
+
+        // Performing and printing calculations
+        int sum = number + 5;
+        System.out.println("Sum of number and 5: " + sum);
+
+        // Boolean value
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
 
