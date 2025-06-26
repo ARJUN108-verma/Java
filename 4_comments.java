8b08783110629ad3b1d3498f4e91d0cc21fc0a71
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-26 11:15:09 +0530

    Create 4_comments.java

---
+++ 4_comments.java
@@ -0,0 +1,33 @@
+// This is a single-line comment in Java
+
+/*
+ * This is a multi-line comment.
+ * It can span multiple lines.
+ */
+
+ /**
+  * This is a documentation comment.
+  * It is used to generate documentation using Javadoc.
+  * This class demonstrates different types of comments.
+  */
+public class CommentExample {
+
+    /**
+     * This is the main method which is the entry point of any Java program.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Demonstrating different types of comments in Java");
+
+        int a = 10; // Declare and initialize an integer variable
+
+        /*
+         * Perform a simple arithmetic operation
+         */
+        int b = a * 2;
+
+        // Print the result
+        System.out.println("Value of b (a * 2): " + b);
+    }
+}
