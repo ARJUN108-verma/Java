3bb5635ccf253f9de7add1bf802b843720c875a6
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-26 11:29:57 +0530

    Create 13_while_loop.java

---
+++ 13_while_loop.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialization
+
+        // While loop: runs as long as i <= 5
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++; // Increment
+        }
+    }
+}
